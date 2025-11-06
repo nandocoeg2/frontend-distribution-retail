f642d85489590941efc6e2fd36b44744e33ba5cb
Fernando Julian <nandokun69@gmail.com>
2025-11-06 22:22:39 +0700

    feat(src/pages): remove delete faktur button

--- src/pages/FakturPajak.jsx
+++ src/pages/FakturPajak.jsx
@@ -186,13 +186,6 @@ const FakturPajakPage = () => {
                 <HeroIcon name='archive-box' className='w-5 h-5 mr-2' />
                 Export e-Faktur DJP
               </button>
-              <button
-                onClick={openCreateModal}
-                className='inline-flex items-center px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700'
-              >
-                <HeroIcon name='plus' className='w-5 h-5 mr-2' />
-                Tambah Faktur Pajak
-              </button>
             </div>
           </div>
 
