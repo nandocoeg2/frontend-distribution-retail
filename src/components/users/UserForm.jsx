9fb8c4e1b17e3b07a71d583a42af36443430789c
Fernando Julian <nandokun69@gmail.com>
2026-01-07 23:16:07 +0700

    feat: Add new form components for Term of Payments and Users.

--- src/components/users/UserForm.jsx
+++ src/components/users/UserForm.jsx
@@ -85,6 +85,7 @@ const UserForm = ({ formData, handleInputChange, handleSubmit, closeModal, isSub
             value={formData.password}
             onChange={handleInputChange}
             required
+            minLength={6}
             className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
             placeholder='Enter password'
           />
