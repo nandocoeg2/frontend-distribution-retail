538ce90ad8764c25b4a76e7389e34dc753d3eb40
divinecoid <admin@divine.co.id>
2026-08-15 10:06:00 +0700

    feat: add onAssignDocument to MutasiBankTableServerSide dependency array

--- src/components/mutasiBank/MutasiBankTableServerSide.jsx
+++ src/components/mutasiBank/MutasiBankTableServerSide.jsx
@@ -527,6 +527,7 @@ const MutasiBankTableServerSide = forwardRef(({
   }, [
     onValidateMutation,
     onViewMutation,
+    onAssignDocument,
     onUnassignDocument,
     isValidating,
     isAssigning,
