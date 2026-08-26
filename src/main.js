c0add84ef52bad2cfe6105a6ccbfeb6b679c7e1d
divinecoid <admin@divine.co.id>
2026-08-26 13:09:21 +0700

    feat: initialize Electron main process with CSP configuration and update app version to 1.0.6

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const createWindow = () => {
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    title: 'ERP PT DOVEN TRADECO v1.0.5',
+    title: 'ERP PT DOVEN TRADECO v1.0.6',
     width: 800,
     height: 600,
     webPreferences: {
