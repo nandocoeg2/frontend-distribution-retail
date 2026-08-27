7bcc9ac8c419de5ff3c44420e89ec6f5ef997790
divinecoid <admin@divine.co.id>
2026-08-27 23:19:16 +0700

    feat: initialize main process, add HTML templates, and bump version to 1.0.9

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const createWindow = () => {
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    title: 'ERP PT DOVEN TRADECO v1.0.8',
+    title: 'ERP PT DOVEN TRADECO v1.0.9',
     width: 800,
     height: 600,
     webPreferences: {
