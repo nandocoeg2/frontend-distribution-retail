c507b4124fc5599dbffbfe92c16075b3dcee71e1
divinecoid <admin@divine.co.id>
2026-08-29 18:20:42 +0700

    feat: initialize main electron process and application entry templates at version 1.0.12

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const createWindow = () => {
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    title: 'ERP PT DOVEN TRADECO v1.0.11',
+    title: 'ERP PT DOVEN TRADECO v1.0.12',
     width: 800,
     height: 600,
     webPreferences: {
